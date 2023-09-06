1f3f4c9bac05317724048738b0a665c4bb36c2ca
ChickenEgg <ytae439@naver.com>
2023-09-06 11:09:15 +0900

    2023-09-06 11:09

--- cate/js/home.js
+++ cate/js/home.js
@@ -5,6 +5,15 @@ jQuery(document).ready(function(){
     }).mouseout(function(){
         $(this).find('.submenu').stop().slideUp(300);
     });
+
+    setInterval(function(){
+        $('.slidelist').delay(3000)
+        $('.slidelist').animate({marginLeft: "-100%"})
+        $('.slidelist').delay(3000)
+        $('.slidelist').animate({marginLeft: "-200%"})
+        $('.slidelist').delay(3000)
+        $('.slidelist').animate({marginLeft: 0})
+    });
 	
 	var myFullpage = new fullpage('#fullpage', {
 		showActiveTooltip: false
